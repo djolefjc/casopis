b56cf044e54b832a5962f855b3a4fca6b00a0205
djolefjc <djolefjc@gmail.com>
2018-03-16 16:46:16 +0100

    Comment section finished, just CSS

--- includes/comment_form.php
+++ includes/comment_form.php
@@ -1,9 +1,56 @@
+<?php
+
+  include('includes/database.php');
+
+                        if(isset($_GET['post'])) {
+
+                          $post_id = $_GET['post'];
+
+
+                          $run_posts = mysqli_query($con,  "SELECT * FROM posts WHERE post_id = '$post_id'");
+
+                          $row = mysqli_fetch_array($run_posts);
+
+                          $post_new_id = $row['post_id'];
+                        }
+
+                        $get_comments = "SELECT * FROM comments WHERE post_id= '$post_new_id' AND status='approve'";
+
+                        $run_comments = mysqli_query($con,$get_comments) or die(mysqli_error($con));
+
+                        while($row_comments = mysqli_fetch_array($run_comments)) {
+
+                          $comment_name = $row_comments['com_name'];
+                          $comment_text = $row_comments['com_text'];
+
+                          echo "
+
+                          <div class='comments'>
+
+                            <div class='comments-name'><h3>$comment_name kaze:</h3></div>
+                            <br />
+                            <div class='comments-text'><p>$comment_text</p></div>
+
+                          </div>
+
+
+
+                          ";
+
+                        }
+ ?>
+
+
+
+
+
+
 <div class="comment">
 <span class="comment-title">
 Postavi komentar:
 </span>
 
- <form methoc="post" action="details.php?post=<?php echo $post_id; ?>">
+ <form method="post" action="details.php?post=<?php echo $post_new_id; ?>">
 
    <div class ="comment-email">
      <span class="com-email">Email:</span>
@@ -19,7 +66,7 @@ Postavi komentar:
    </div>
    <div class="comment-text">
      <span id="com-input2"
-     ><textarea name="comment" cols="60" rows="20" />  </textarea>
+     ><textarea name="comment_text" cols="60" rows="20" />  </textarea>
    </span>
    </div>
    <div class="com-button">
@@ -29,18 +76,36 @@ Postavi komentar:
 
 <?php
 
-if(isset($_POST['comment'])) {
 
-  $comment_name = $_POST['coment_name'];
-  $comment_email = $POST['comment_email'];
-  $comment = $_POST['comment'];
-  $status = "deny";
+if(isset($_POST['comment_text'])) {
 
-  if($comment_email=='' or $comment == '') {
-    echo "<script>alert('Please fill in all the blanks')</script>";
+  $post_com_id = $post_new_id;
+  $comment_name = $_POST['comment_name'];
+  $comment_email = $_POST['comment_email'];
+  $comment_text = $_POST['comment_text'];
+  $status = "unapprove";
+  $comment_date = date('d-m-y');
+
+  if($comment_email=='' or $comment_text == '') {
+    echo "<script>alert('Molimo Vas popunite sva polja')</script>";
     echo"<scripit>window.open('details.php?post=$post_id')</scripit>";
     exit();
   }
+
+  else {
+    $query_comment = "INSERT INTO comments(post_id, com_email, com_name,
+    com_text, com_date, status) VALUES ('$post_com_id','$comment_email','$comment_name',
+    '$comment_text','$comment_date','$status')";
+
+
+    if(mysqli_query($con, $query_comment)) {
+      echo "<script>alert('Vas komentar ce biti izbacen posle odobrenja.')</script>";
+      echo"<script>window.open('details.php?post=$post_id')</script>";
+    }
+
+  }
+
+
 
 }
 
